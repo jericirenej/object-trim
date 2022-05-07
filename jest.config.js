c26407d979c4988e7aec58801ae998ac93381dc5
jericirenej <63595376+jericirenej@users.noreply.github.com>
2022-05-07 16:26:37 +0200

    Initial commit.

---
+++ jest.config.js
@@ -0,0 +1,4 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+export const preset = "ts-jest/presets/js-with-ts-esm";
+export const testEnvironment = "node";
+export const maxWorkers = 1;
