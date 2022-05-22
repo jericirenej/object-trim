5ef6f12c8e0b7786a0b5bd610d26f8146ed85cef
jericirenej <63595376+jericirenej@users.noreply.github.com>
2022-05-22 16:17:11 +0200

    - Previous filterExecution functions deleted in favor of the recursiveFilter function. - objectFilter will now call the filter fomratting function if earlyReturnChecks pass.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 "use strict";
 
 import {
-  earlyReturnChecks,
-  EXCLUDED_TYPES,
-  filterByRegex,
-  formatFilters
+  earlyReturnChecks, formatFilters,
+  recursiveFilter
 } from "./utils.js";
 
 // Type declarations
@@ -18,7 +16,7 @@ export interface ObjectFilterArgs {
 }
 
 /** Filter an object based on matching its key against the provided filters.
- *  Supply a configuration object with *targetObject*, *filterType*, and *filters* properties.
+ * Filtering can be *flat* or *recursive* (default).
  * Two types of filtering available:
  *  * *include*: includes only the properties that match the filter keys.
  *  * *exclude*: excludes the properties that match filter keys.
@@ -27,72 +25,19 @@ const objectFilter = ({
   targetObject,
   filters,
   regexFilters,
-  filterType,
+  filterType = "exclude",
   recursive = true,
 }: ObjectFilterArgs): Record<string, any> => {
   const args = { targetObject, filters, regexFilters, filterType };
   if (earlyReturnChecks(args)) return targetObject;
-  if (recursive) {
-    return executeRecursiveFilter(args);
-  }
-  return executeObjectFilter(args);
-};
-
-const executeObjectFilter = (
-  config: Omit<ObjectFilterArgs, "recursive">
-): Record<string, any> => {
-  const filteredObject: Record<string, any> = {};
-  const { filters, targetObject, filterType, regexFilters } = config;
-  const objKeys = Object.keys(targetObject);
-
   const { filterKeys, regexKeys } = formatFilters(filters, regexFilters);
-
-  const checkedFilterKeys = filterKeys.filter((key: string) =>
-    objKeys.includes(key)
-  );
-  // !No longer need to filter out those object keys, covered by filterKeys!
-  // !Quite the opposite: regexFiltering should precede normal filters!
-  // TODO: However, this implementation will be replaced with the ExtractProperty function!
-  const checkedRegexKeys = regexKeys.length
-    ? filterByRegex(
-        objKeys.filter(key => !checkedFilterKeys.includes(key)),
-        regexKeys
-      )
-    : [];
-  if (!(checkedFilterKeys.length || checkedRegexKeys.length))
-    return targetObject;
-
-  const targetKeys = objKeys.filter(key => {
-    const isAmongCheckedKeys =
-      checkedFilterKeys.includes(key) || checkedRegexKeys.includes(key);
-    return filterType === "include" ? isAmongCheckedKeys : !isAmongCheckedKeys;
+  return recursiveFilter({
+    sourceObject: targetObject,
+    filterType,
+    filterKeys,
+    regexKeys,
+    recursive,
   });
-
-  targetKeys.forEach(key => (filteredObject[key] = targetObject[key]));
-
-  return filteredObject;
-};
-
-const executeRecursiveFilter = (
-  config: ObjectFilterArgs
-): Record<string, any> => {
-  const filterAndEvaluate = (config: ObjectFilterArgs): Record<string, any> => {
-    const filteredObject = executeObjectFilter(config);
-    for (const name in filteredObject) {
-      const property = filteredObject[name];
-      const isValidObject =
-        EXCLUDED_TYPES.every(type => !(property instanceof type)) &&
-        typeof property === "object";
-      if (isValidObject) {
-        filteredObject[name] = filterAndEvaluate({
-          ...config,
-          targetObject: property,
-        });
-      }
-    }
-    return filteredObject;
-  };
-  return filterAndEvaluate(config);
 };
 
 export default objectFilter;
