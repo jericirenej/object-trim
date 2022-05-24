a4fb827e68a2dd10d12db360bac2c6f3f54ace31
jericirenej <63595376+jericirenej@users.noreply.github.com>
2022-05-24 20:27:42 +0200

    - Transpiling now done via single configuration file (switch is performed by NODE_ENV setting). - Removed dist/helpers from .npmignore, as helpers no longer get transpiled.

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,14 +1,28 @@
 module.exports = function configureBabel(api) {
   api.cache(true);
+  const esModule = process.env.NODE_ENV.trim() === "esmodule";
+  const config = {
+    modules: esModule ? false : "cjs",
+    extension: esModule ? "mjs" : "cjs",
+  };
   const ignore = ["src/tests", "src/helpers"];
 
   const presets = [
-    ["@babel/preset-env", { targets: { node: "14.17.0" }, modules: false }],
+    [
+      "@babel/preset-env",
+      {
+        targets: { node: "14.17.0" },
+        modules: config.modules,
+      },
+    ],
     ["@babel/preset-typescript"],
   ];
 
   const plugins = [
-    ["babel-plugin-add-import-extension", { extension: "mjs", replace: true }],
+    [
+      "babel-plugin-add-import-extension",
+      { extension: config.extension, replace: true },
+    ],
   ];
   return { presets, plugins, ignore };
 };
